f78bd241cc6c33d603fe56ab93ff28fe9b6cc8e9
Mohamed Amine Naimi <mon@trifork.com>
2022-07-22 14:45:15 +0200

    First working unit test! Unit test adding an expense

--- src/test/java/ExpenseTrackerTest.java
+++ src/test/java/ExpenseTrackerTest.java
@@ -1,19 +1,22 @@
 import models.Expense;
 import models.Person;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
+import java.util.List;
 
 public class ExpenseTrackerTest {
+    private ExpenseTrackerService expenseTrackerService = new ExpenseTrackerServiceImpl();
     @Test
     public void Test_AddExpense() {
         Person person = new Person("Jesper");
         Expense expense = new Expense(new BigDecimal(1000.0), "this is fun", person);
-        Interface.addExpense(expense);
-        ArrayList<Expense> expenses = Interface.getExpenses();
-        Assertions.assertEquals(expenses.contains(expense), "nice");
+                expenseTrackerService.addExpense(expense);
+
+        List<Expense> expenses = expenseTrackerService.getExpenses();
+        Assert.assertTrue(expenses.contains(expense));
+        Assert.assertEquals(1,expenses.size());
     }
 
 }
